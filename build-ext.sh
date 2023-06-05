aed4ae2ad307b4da206a8fb13938c5fb25b45608
yaya2007 <8120248+yaya2007@users.noreply.github.com>
2023-06-05 15:40:23 +0800

    Update build-ext.sh

--- build-ext.sh
+++ build-ext.sh
@@ -5,7 +5,7 @@ git clone --depth=1 https://github.com/chenall/grubutils.git $GITHUB_WORKSPACE/g
 pushd $GITHUB_WORKSPACE/grubutils
 GRUB4DOS_VER=$(cat $GITHUB_WORKSPACE/grub4dos_version)
 if [ "${GRUB4DOS_VER/EFI}" = "${GRUB4DOS_VER}" ]; then
-  sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.8 100
+  sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-11 100
   make -C umbr
   make -j -C g4dext && mv g4dext/bin g4dext/ext
   make -C g4dext clean
